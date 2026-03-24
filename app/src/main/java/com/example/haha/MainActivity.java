538db72b47bc7f0902ba3d17662d626f84d54a00
Sposecret <fuesiclear@gmail.com>
2026-03-24 16:03:56 +0100

    Create MainActivity.java with WebView to load index.html from assets

---
+++ app/src/main/java/com/example/haha/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.haha;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = findViewById(R.id.webview);
+        myWebView.loadUrl("file:///android_asset/index.html");
+    }
+}
